c6db13b165b7c0a8fdf42f91a30169dd1fb47a44
Nazar Hladaniuk <powerlolek46@gmail.com>
2026-04-16 20:39:18 +0300

    view test

--- directory/views.py
+++ directory/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Employee
 
-# Create your views here.
+
+def employee_list(request):
+    query = request.GET.get('q')
+
+    employees = Employee.objects.all()
+
+    if query:
+        employees = employees.filter(
+            last_name__icontains=query
+        )
+
+    return render(request, 'directory/employee_list.html', {
+        'employees': employees
+    })
