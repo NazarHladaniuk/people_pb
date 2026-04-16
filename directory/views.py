0de233c36201864e4775f6a876b52b7ff5b29844
Nazar Hladaniuk <powerlolek46@gmail.com>
2026-04-16 21:09:00 +0300

    better view for test

--- directory/views.py
+++ directory/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Employee
 
 
 def employee_list(request):
-    query = request.GET.get('q')
+    query = request.GET.get('q', '').strip()
 
     employees = Employee.objects.all()
 
     if query:
         employees = employees.filter(
-            last_name__icontains=query
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(middle_name__icontains=query) |
+            Q(phone__icontains=query) |
+            Q(email__icontains=query)
         )
 
+    if request.headers.get('HX-Request'):
+        return render(request, 'directory/partials/employee_cards.html', {
+            'employees': employees
+        })
+
     return render(request, 'directory/employee_list.html', {
-        'employees': employees
+        'employees': employees,
+        'query': query,
     })
